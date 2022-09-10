99fd8507163459198a2b20425944922bfaae8800
hajjajifaycel <63471102+hajjajifaycel@users.noreply.github.com>
2022-09-10 04:56:20 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <stopwatch :running="running" :resetWhenStart="true" />
+    <button @click="startT">start</button> <button @click="stopT">stop</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Stopwatch from "./components/StopWatch";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Stopwatch
+  },
+  methods: {
+    stopT: function() {
+      this.running = false;
+    },
+    startT: function() {
+      this.running = true;
+    }
+  },
+  data() {
+    return {
+      running: true
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
